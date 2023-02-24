03bf2a38c75cae47b5e4eb0bd285cf00645e84a6
James Mahfouz <jamesmahfouz0@gmail.com>
2023-02-24 20:01:22 +0200

    Added some comments and the read me file

--- Dice-game-changing_pictures/dice.js
+++ Dice-game-changing_pictures/dice.js
@@ -1,15 +1,17 @@
 window.onload = function(){
     let first_dice = document.getElementById('first_dice')
     let second_dice = document.getElementById('second_dice')
+    let status = document.getElementById('status')
 
+    //generating two numbers between 1 and 6
     let dice_1 = Math.floor(Math.random() * 6) + 1;
     let dice_2 = Math.floor(Math.random() * 6) + 1;
-
-    let status = document.getElementById('status')
     
+    //changing the image source depending on the number generated
     first_dice.src = "images/dice-" + dice_1 + ".png"
     second_dice.src = "images/dice-" + dice_2 + ".png"
 
+    //checking the bigger number to display the winner
     if (dice_1 > dice_2){
         status.innerHTML = "&#129322 Player 1 Wins!"
     }else if (dice_1<dice_2){
---
+++ README.md
@@ -0,0 +1,13 @@
+## About
+
+This is a dice game made in two way the first way is by changing the oppacity of dots  on the dice with javascript the second one is by simplly changing the source of the image depending on the number.
+
+## Languages
+
+- HTML
+- CSS
+- Javascript
+
+## Author
+
+- James Mahfouz
